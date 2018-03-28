01f10e7ddfcf4b0bcf8617b1c24c0751c7118208
Kasey <KaseyColian@gmail.com>
2018-03-27 22:30:39 -0400

    added JS for nav links

--- src/js/portfolioApp.js
+++ src/js/portfolioApp.js
@@ -1,10 +1,7 @@
-const asideNav = document.querySelector('nav');
+const navigation = document.getElementById('navigation');
 
 const navMenu = document.querySelector('nav ul');
 const nav = document.querySelector('nav');
-const aside = document.querySelector('aside');
-const projectInfo = document.querySelector('content h3');
-const container = document.getElementById('container');
 const headerText = document.getElementById('header');
 
 button = document.createElement('button');
@@ -14,15 +11,13 @@ button.classList.add('hide-side-button');
 
 showButton = document.createElement('button');
 showButton.innerHTML = 'Show Nav';
-headerText.appendChild(showButton);
+container.appendChild(showButton);
 showButton.classList.add('show-nav-button');
 showButton.setAttribute("id", "show-button");
 showButton.style.display="none";
 
 
 const projectName = document.querySelector('.project h3');
-
-
 const leftButton = document.querySelector('.left');
 const rightButton =document.querySelector('.right');
 const projectButton = document.querySelector('.btn');
@@ -30,14 +25,50 @@ const projectButton = document.querySelector('.btn');
 //button to show/hide project details
 // const projectContent = document.querySelector('#projectContent');
 
+const inner = document.getElementById('inner');
+const footer = document.getElementById('footer');
+
+//hiding side:
+button.addEventListener('click', function(){
+	navigation.style.display = "none";
+	showButton.style.display = "block";
+	inner.style.marginLeft = "-14rem";
+	footer.style.paddingLeft="1rem";
+	// navigation.transition= "none 5s";
+
+});
+
+//showing side:
+showButton.addEventListener('click', function(){
+
+	navigation.style.display = "block";
+	document.getElementById('show-button').style.display = "none";
+	inner.style.marginLeft = "0rem";
+	footer.style.paddingLeft = "15rem";
+
+});
+
+//modal box
+const modal = document.getElementById('modalBox');
+const modalLink = document.getElementById('modalLink');
+const closeLink = document.getElementsByClassName("close")[0];
+
+modalLink.addEventListener("click", function (){
+	modal.style.display = "block";
+});
+
+closeLink.addEventListener("click", function() {
+	modal.style.display = "none";
+});
+
+
+//project carousel
 
 const projectInfoButton = document.querySelector('.projectDetails');
 const projectInfoButtons = document.querySelectorAll('projectDetails');
 
 for (var i = 0; i<projectInfoButtons.length; i++){
 	projectInfoButtons[i].addEventListener('click', function(){
-		const projectInfoButtons = document.querySelectorAll('.projectDetails');
-		const allProjectContent = document.querySelectorAll('.projectContent');
 
 		let reviewContent = document.getElementById(this.dataset.contentid);
 
@@ -51,34 +82,7 @@ for (var i = 0; i<projectInfoButtons.length; i++){
 		}
 	});
 }
-const inner = document.getElementById('inner');
-//hiding side:
-button.addEventListener('click', function(){
-	document.getElementById('navigation').style.display = "none";
-	showButton.style.display = "block";
-	document.getElementById('inner').style.marginLeft = "-14rem";
-	document.getElementById('footer').style.paddingLeft="1rem";
-	// navigation.transition= "none 5s";
 
-
-
-});
-//showing side:
-showButton.addEventListener('click', function(){
-
-	// document.getElementById('container').style.marginLeft = "5rem";
-	document.getElementById('navigation').style.display = "block";
-	// document.getElementById('hide-side-button').style.display = "inline";
-	document.getElementById('show-button').style.display = "none";
-	document.getElementById('inner').style.marginLeft = "0rem";
-	document.getElementById('footer').style.paddingLeft="15rem";
-
-
-
-});
-
-
-//project carousel
 var slide_index = 1;  
 displaySlides(slide_index);  
 function nextSlide(n) {  
@@ -112,16 +116,13 @@ function displaySlides(n) {
 	}  
 	slides[slide_index].style.display = "block";  
 }
+const listItems = document.querySelectorAll('nav ul li a');
+listItems.forEach(function(listItem) {
+	listItem.addEventListener('mouseenter', function() {
+		this.style.fontSize = '1.5rem';
+	})
+	listItem.addEventListener('mouseout', function(){
+		this.style.fontSize = '1.2rem';
+	})
+})
 
-//modal box
-const modal = document.getElementById('modalBox');
-const modalLink = document.getElementById('modalLink');
-const closeLink = document.getElementsByClassName("close")[0];
-
-modalLink.addEventListener("click", function (){
-	modal.style.display = "block";
-});
-
-closeLink.addEventListener("click", function() {
-	modal.style.display = "none";
-});
